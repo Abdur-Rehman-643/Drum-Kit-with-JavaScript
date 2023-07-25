90c27ab39f350b0be2b62a7bc0d07ab95f7c899e
ABDUR REHMAN <rehmanabdur643@gmail.com>
2023-07-25 11:37:31 +0500

    RESOLVE ERRORS

--- sources/index.js
+++ sources/index.js
@@ -1,32 +1,7 @@
 let drumsarray = document.querySelectorAll(".drum");
 
 function playSound(keyword) {
-    var audio;
-    switch (keyword) {
-        case 'w':
-            audio = new Audio("/sounds/crash.mp3");
-            break;
-        case 'a':
-            audio = new Audio("/sounds/kick-bass.mp3");
-            break;
-        case 's':
-            audio = new Audio("/sounds/snare.mp3");
-            break;
-        case 'd':
-            audio = new Audio("/sounds/tom-1.mp3");
-            break;
-        case 'j':
-            audio = new Audio("/sounds/tom-2.mp3");
-            break;
-        case 'k':
-            audio = new Audio("/sounds/tom-3.mp3");
-            break;
-        case 'l':
-            audio = new Audio("/sounds/tom-4.mp3");
-            break;
-        default:
-            return;
-    }
+    var audio = new Audio("sounds/" + keyword + ".mp3");
     audio.play();
 }
 
